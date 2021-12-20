9afa869b5bab0b5c7192bd52b84a40c97a1c9296
andrel <andrelchew@icloud.com>
2021-12-20 20:11:55 +0800

    updates

--- app.py
+++ app.py
@@ -3,17 +3,20 @@ import pandas as pd
 from predictor import predict, load_model
 
 st.set_page_config(
-    page_title="MBTI 유형",
-    page_icon="🎉"
+    page_title="Myer-Brigging this up...",
+    page_icon="🔮"
 )
 
 st.write("""# MBTI Prediction Tool""")
 # refer to : https://docs.streamlit.io/library/api-reference
 # refer to : https://docs.streamlit.io/en/stable/api.html#magic-commands
 
+with st.spinner("Loading the genie..."):
+	model = load_model()
+
 st.write('*The Data*! :sunglasses:')
 df = pd.read_csv("https://drive.google.com/uc?id=1NWGrm705AS3xOWrDFfETswf2saoPCkyu")
-# df = df.head(100)
+df = df.sample(frac=1).reset_index(drop=True)
 df
 # st.dataframe(df) 
 
@@ -23,16 +26,26 @@ df
 # df = df.head(1000)
 # df
 
-with st.spinner("Loading AI..."):
-	model = load_model()
+# if st.checkbox("Show dataset with selected columns"):
+#     # get the list of columns
+#     columns = df.columns.tolist()
+#     st.write("#### Select the columns to display:")
+#     selected_cols = st.multiselect("", columns)
+#     if len(selected_cols) > 0:
+#         selected_df = df[selected_cols]
+#         st.dataframe(selected_df.value_counts())
+
+# st.write(df.label.value_counts())
 
 st.subheader("Let's guess your MBTI...")
 user_input = st.text_area(label= "Enter some text here:" , help = "Type something here, then click anywhere outside the box!")
+btn = st.button("Guess!")
 
-if user_input != "":
-    prediction = predict(model, user_input)
+if (user_input != "") or btn:
+    with st.spinner("Hmmm..."):
+        prediction = predict(model, user_input)
     print(prediction)
-    st.write("The genie thinks you're an ", prediction, "🥰")
+    st.write("The genie thinks you're an ", prediction, "🔮")
     # st.balloons()
 
 # btn = st.button("Party!")
